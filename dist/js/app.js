ae4e44b6a2bc81d0710b193a5034e0487feb8a40
lil smoke <artemgarbach99@gmail.com>
2022-11-16 08:10:14 +0300

    delete burger menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -18,9 +18,6 @@
         if (location.hash) return location.hash.replace("#", "");
     }
     let bodyLockStatus = true;
-    let bodyLockToggle = (delay = 500) => {
-        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
-    };
     let bodyUnlock = (delay = 500) => {
         let body = document.querySelector("body");
         if (bodyLockStatus) {
@@ -39,30 +36,6 @@
             }), delay);
         }
     };
-    let bodyLock = (delay = 500) => {
-        let body = document.querySelector("body");
-        if (bodyLockStatus) {
-            let lock_padding = document.querySelectorAll("[data-lp]");
-            for (let index = 0; index < lock_padding.length; index++) {
-                const el = lock_padding[index];
-                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            }
-            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            document.documentElement.classList.add("lock");
-            bodyLockStatus = false;
-            setTimeout((function() {
-                bodyLockStatus = true;
-            }), delay);
-        }
-    };
-    function menuInit() {
-        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
-            if (bodyLockStatus && e.target.closest(".icon-menu")) {
-                bodyLockToggle();
-                document.documentElement.classList.toggle("menu-open");
-            }
-        }));
-    }
     function menuClose() {
         bodyUnlock();
         document.documentElement.classList.remove("menu-open");
@@ -3639,6 +3612,5 @@
     }), 0);
     window["FLS"] = true;
     isWebp();
-    menuInit();
     pageNavigation();
 })();
